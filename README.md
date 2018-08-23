57282fbb5a4c13b2d7a1a3f18f2e1b0ab08d7b99
Yifong.Jiang <forink@gmail.com>
2018-08-23 20:58:14 +0800

    v 0.2.0 - Add the line chart supports.

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ The D3 Instant Charts relies on jquery 3.x and D3.js v5 libraries for rapid deve
 
 ##Changelog
 
+v 0.2.0 - Add the line chart supports.
+
 v 0.1.0 - Launched. Add the bar chart supports.
--- js/d3-instant-charts.js
+++ js/d3-instant-charts.js
@@ -12,7 +12,7 @@
 /*eslint no-extra-parens: [2, "functions"]*/
 
 (function ($) {
-    "use strict";
+    'use strict';
     var defaults = {
     };
 
@@ -24,19 +24,19 @@
             useClientSize: false,  //This plugin will detect the client size to autofit the svg size.
             width: 400,  //svg width
             height: 300,  //svg height
-            marginTop: 30,  //svg margin top
-            marginRight: 30,  //svg margin right
-            marginButtom: 30,  //svg margin buttom
-            marginLeft: 80,  //svg margin left
+            marginTop: 50,  //svg margin top
+            marginRight: 50,  //svg margin right
+            marginButtom: 50,  //svg margin buttom
+            marginLeft: 50,  //svg margin left
             barSpacing: 0.1,  //設定Bar間距
             barWidthRate: 0.3,  //設定Bar寬比率 (0~1，數字越小越粗)
             axisXScaleCount: 10,  //X軸刻度數量
             toolTipFormat: '{%name%} - {%value%}'
         }, options);
 
-        var targetId = $(this).attr("id");
+        var targetId = $(this).attr('id');
         var jsonObj = callJson(settings.jsonUrl);
-        //console.log(jsonObj.ChartData);
+        //console.log(jsonObj.d3chart);
 
         if (jsonObj === null || jsonObj === 'ERROR') { return; }
 
@@ -70,20 +70,20 @@
             .attr('height', svgHeight);
 
         //建立坐標軸圖層
-        var axisLayer = svg.append("g")
-            .classed("axisLayer", true)
-            .attr("width", svgWidth)
-            .attr("height", svgHeight);
+        var axisLayer = svg.append('g')
+            .classed('axis-layer', true)
+            .attr('width', svgWidth)
+            .attr('height', svgHeight);
 
         //建立主圖表圖層
-        var chartLayer = svg.append("g")
-            .classed("chartLayer", true)
-            .attr("width", chartWidth)
-            .attr("height", chartHeight)
-            .attr("transform", "translate(" + [margin.left, margin.top] + ")");
+        var chartLayer = svg.append('g')
+            .classed('chart-layer', true)
+            .attr('width', chartWidth)
+            .attr('height', chartHeight)
+            .attr('transform', 'translate(' + [margin.left, margin.top] + ')');
 
         //設定資料Root
-        var dataset = jsonObj.ChartData;
+        var dataset = jsonObj.d3chart;
 
         //取得資料最大值
         var maxDataVal = d3.max(dataset, function (d) { return d.value; });
@@ -113,7 +113,7 @@
             .call(d3.axisBottom(xScale)
                 .ticks()
                 .tickSizeInner(-chartHeight)
-                .tickFormat("")
+                .tickFormat('')
             );
 
         //繪製X軸
@@ -151,8 +151,8 @@
                 tooltip.transition().duration(200)
                     .style('opacity', 0.8);
                 tooltip.html(String(settings.toolTipFormat)
-                        .replace("{%name%}", d.name)
-                        .replace("{%value%}", d.value))
+                        .replace('{%name%}', d.name)
+                        .replace('{%value%}', d.value))
                     .style('left', (d3.event.pageX) + 'px')
                     .style('top', (d3.event.pageY - 28) + 'px');
                 d3.select(this)
@@ -216,12 +216,245 @@
 
         // Default options
         var settings = $.extend({
-            name: 'John Doe',
-            color: 'orange'
+            jsonUrl: '',
+            useClientSize: false,  //This plugin will detect the client size to autofit the svg size.
+            width: 400,  //svg width
+            height: 300,  //svg height
+            marginTop: 50,  //svg margin top
+            marginRight: 50,  //svg margin right
+            marginButtom: 50,  //svg margin buttom
+            marginLeft: 50,  //svg margin left
+            axisYScaleCount: 10,  //Y軸刻度數量
+            toolTipFormat: '{%name%}: {%values.x%} - {%values.y%}'
         }, options);
 
-        // Apply options
-        //this.append(testA());
+        var targetId = $(this).attr('id');
+        var jsonObj = callJson(settings.jsonUrl);
+        //console.log(jsonObj.d3chart);
+
+        if (jsonObj === null || jsonObj === 'ERROR') { return; }
+
+        //設定畫布邊界
+        var margin = {
+            top: settings.marginTop,
+            right: settings.marginRight,
+            bottom: settings.marginButtom,
+            left: settings.marginLeft
+        };
+
+        //設定畫布大小
+        var svgWidth, svgHeight;
+        if (settings.useClientSize) {
+            svgWidth = document.querySelector('#' + targetId).clientWidth;
+            svgHeight = document.querySelector('#' + targetId).clientHeight;
+            if (svgWidth === 0 || svgHeight === 0) {
+
+            }
+        } else {
+            svgWidth = settings.width;
+            svgHeight = settings.height;
+        }
+
+        //設定圖表大小
+        var chartWidth = svgWidth - (margin.left + margin.right);
+        var chartHeight = svgHeight - (margin.top + margin.bottom);
+
+        //建立圖框
+        var svg = d3.select('#' + targetId)
+            .append('svg')
+            .attr('class', 'd3-instant-charts')
+            .attr('width', svgWidth)
+            .attr('height', svgHeight);
+
+        //建立坐標軸圖層
+        var axisLayer = svg.append('g')
+            .classed('axis-layer', true)
+            .attr('width', svgWidth)
+            .attr('height', svgHeight);
+
+        //建立主圖表圖層
+        var chartLayer = svg.append('g')
+            .classed('chart-layer', true)
+            .attr('width', chartWidth)
+            .attr('height', chartHeight)
+            .attr('transform', 'translate(' + [margin.left, margin.top] + ')');
+
+        //設定資料Root
+        var dataset = jsonObj.d3chart;
+
+        //日期資料Parse格式
+        var timeParseFormat = d3.timeParse('%Y-%m');
+        var outputTimeFormat = d3.timeFormat('%Y/%m');
+
+        //處理時間序列資料
+        dataset.forEach(function (d) {
+            d.values.forEach(function (d) {
+                d.x = timeParseFormat(d.x);
+                d.y = +d.y;
+            });
+        });
+
+        //取得資料最大值
+        var maxDataVal = d3.max(dataset, function (d) {
+            return d3.max(d.values, d=>d.y);
+        });
+
+        //刻度值
+        var tickVal = d3.tickStep(0, maxDataVal, settings.axisYScaleCount);
+
+        //計算出X軸最大值
+        var axisYMaxVal = (tickVal - (maxDataVal % tickVal)) + maxDataVal;
+
+        //設定X軸尺度
+        var xScale = d3.scaleTime()
+            .domain(d3.extent(dataset[0].values, d=>d.x))
+            .range([0, chartWidth]);
+
+        //設定Y軸尺度
+        var yScale = d3.scaleLinear()
+            .domain([0, maxDataVal])
+            .range([chartHeight, 0]);
+
+        //繪製 X Grid
+        var gridX = axisLayer.append('g')
+            .attr('class', 'grid-x')
+            .attr('transform', 'translate(' + margin.left + ',' + (chartHeight + margin.top) + ')')
+            .call(d3.axisBottom(xScale)
+                .ticks()
+                .tickSizeInner(-chartHeight)
+                .tickFormat('')
+            );
+
+        //繪製 Y Grid
+        var gridY = axisLayer.append('g')
+            .attr('class', 'grid-y')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+            .call(d3.axisLeft(yScale)
+                .ticks()
+                .tickSizeInner(-chartWidth)
+                .tickFormat('')
+            );
+
+        //繪製X軸
+        var axisX = axisLayer.append('g')
+            .attr('class', 'axis-x')
+            .attr('transform', 'translate(' + margin.left + ',' + (chartHeight + margin.top) + ')')
+            .call(d3.axisBottom(xScale)
+                .ticks()
+                .tickFormat(outputTimeFormat)
+            );
+
+        //繪製Y軸
+        var axisY = axisLayer.append('g')
+            .attr('class', 'axis-y')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+            .call(d3.axisLeft(yScale));
+
+        //滑鼠移過時的Tooltip區塊
+        var tooltip = d3.select('body').append('div')
+            .attr('class', 'd3-instant-charts-tooltip');
+
+        //設定折線顏色
+        var lineColor = d3.scaleOrdinal(d3.schemeCategory10);
+
+        //設定折線綁定
+        var line = d3.line()
+            .x(d=> xScale(d.x))
+            .y(d=> yScale(d.y))
+            .curve(d3.curveMonotoneX);
+
+        //建立折線
+        var lines = chartLayer.append('g')
+            .attr('class', 'lines');
+
+        //繪製折線
+        lines.selectAll('.line-group')
+            .data(dataset)
+            .enter()
+            .append('g')
+            .attr('class', 'line-group')
+            .attr('data-id', d => d.name)
+            .append('path')
+            .attr('class', 'line')
+            .attr('d', d => line(d.values))
+            .style('stroke', (d, i) => lineColor(i))
+            .style('opacity', 0.3)
+            .call(lineTransition)
+            .on('mouseover', function (d) {
+                tooltip.transition().duration(200)
+                    .style('opacity', 0.8);
+                tooltip.html(d.name)
+                    .style('left', (d3.event.pageX) + 'px')
+                    .style('top', (d3.event.pageY - 28) + 'px');
+                d3.select(this).style('opacity', 0.8);
+            })
+            .on('mousemove', function () {
+                return tooltip
+                    .style('top', (d3.event.pageY - 10) + 'px')
+                    .style('left', (d3.event.pageX + 10) + 'px');
+            })
+            .on('mouseout', function (d) {
+                tooltip.transition().duration(500).style('opacity', 0);
+                d3.select(this).style('opacity', 0.3);
+            });
+
+        //繪製折線點
+        lines.selectAll('.circle-group')
+            .data(dataset)
+            .enter()
+            .append('g')
+            .attr('class', 'circle-group')
+            .attr('data-id', d => d.name)
+            .style('fill', (d, i) => lineColor(i))
+            .selectAll('circle')
+            .data(d => d.values)
+            .enter()
+            .append('g')
+            .attr('class', 'circle')
+            .append('circle')
+            .attr('cx', d => xScale(d.x))
+            .attr('cy', d => yScale(d.y))
+            .attr('r', 0)
+            .style('opacity', 0.6)
+            .on('mouseover', function (d) {
+                tooltip.transition().duration(200)
+                    .style('opacity', 0.8);
+                tooltip.html(String(settings.toolTipFormat)
+                        .replace('{%name%}', d3.select(this.parentNode.parentNode).datum().name)
+                        .replace('{%values.x%}', outputTimeFormat(d.x))
+                        .replace('{%values.y%}', d.y))
+                    .style('left', (d3.event.pageX) + 'px')
+                    .style('top', (d3.event.pageY - 28) + 'px');
+                d3.select(this)
+                    .style('opacity', 1)
+                    .style('stroke-width', '2px');
+            })
+            .on('mousemove', function () {
+                return tooltip.style('top', (d3.event.pageY - 10) + 'px')
+                    .style('left', (d3.event.pageX + 10) + 'px');
+            })
+            .on('mouseout', function (d) {
+                tooltip.transition().duration(500).style('opacity', 0);
+                d3.select(this).style('opacity', 0.6).style('stroke-width', '0');
+            })
+            .transition()
+            .duration(2800)
+            .attr('r', 6);
+
+        //繪製圖例
+        //繼續努力
+
+        //設定折線漸變效果
+        function lineTransition(line) {
+            line.transition().duration(2000).attrTween("stroke-dasharray", calcTween);
+        }
+
+        //計算折線方位
+        function calcTween() {
+            var len = this.getTotalLength();
+            var ips = d3.interpolateString("0," + len, len + "," + len);
+            return function (t) { return ips(t); };
+        }
     };
 
     //$.fn.areaChart = function (options) { };
@@ -231,10 +464,6 @@
     //$.fn.radarChart = function (options) { };
 
     //$.fn.gaugeChart = function (options) {};
-
-    var testA = function () {
-        return 'Hello ' + defaults.holidayColor + '!';
-    };
 
     //Get json data from url
     var callJson = function (url) {
